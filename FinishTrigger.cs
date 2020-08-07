b0b0105b10fd4a87818ecee3754044619078e14d
cazterk <cazterk@github.com>
2020-08-07 19:25:14 +0200

    added game menus

---
+++ FinishTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishTrigger : MonoBehaviour
+{
+
+    public GameObject completeLevelUI;
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+
+}
---
+++ LoadNextEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextEvent : MonoBehaviour
+{
+
+    public void CompleteLeve()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
